c33a4ae7daf6168c15f677f7de00b48abba644b4
Jason Mullings <wjasonmullings@gmail.com>
2021-05-13 19:44:34 -0400

    adjusted initializers

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -12,11 +12,11 @@ if File.exist?(schedule_file) && Sidekiq.server?
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL'), size: 1, network_timeout: 5 }
+  config.redis = { url: ENV.fetch("REDISTOGO_URL"), size: 1, network_timeout: 5 }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL'), size: 12, network_timeout: 5 }
+  config.redis = { url: ENV.fetch("REDISTOGO_URL"), size: 12, network_timeout: 5 }
 end
 
 Sidekiq::Extensions.enable_delay!
