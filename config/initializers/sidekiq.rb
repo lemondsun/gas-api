94a23672446f90eaaca35c4ecdfc86a23c9c54fb
Jason Mullings <wjasonmullings@gmail.com>
2021-05-13 18:39:22 -0400

    added heroku variables and set up sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -10,3 +10,13 @@ schedule_file = "config/schedule.yml"
 if File.exist?(schedule_file) && Sidekiq.server?
   Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
 end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV.fetch('REDIS_URL'), size: 1, network_timeout: 5 }
+end
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV.fetch('REDIS_URL'), size: 12, network_timeout: 5 }
+end
+
+Sidekiq::Extensions.enable_delay!
