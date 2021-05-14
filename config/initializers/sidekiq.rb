c45fa04be64bfa48b2721d0d22ee34ad12895aa3
Jason Mullings <wjasonmullings@gmail.com>
2021-05-14 01:02:02 -0400

    change concurrency to 3

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -16,7 +16,7 @@ Sidekiq.configure_client do |config|
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch("REDISTOGO_URL"), size: 12, network_timeout: 5 }
+  config.redis = { url: ENV.fetch("REDISTOGO_URL"), size: 10, network_timeout: 5 }
 end
 
 Sidekiq::Extensions.enable_delay!
