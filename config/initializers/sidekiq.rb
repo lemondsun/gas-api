1314b9d56a755315c4f26838ae499e065e7d1cff
Jason Mullings <wjasonmullings@gmail.com>
2021-05-15 02:25:58 -0400

    adjusted email styling

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -11,12 +11,12 @@ if File.exist?(schedule_file) && Sidekiq.server?
   Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
 end
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch('REDISTOGO_URL'), size: 10, network_timeout: 5 }
-end
+# Sidekiq.configure_client do |config|
+#   config.redis = { url: ENV.fetch('REDISTOGO_URL'), size: 10, network_timeout: 5 }
+# end
 
-Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch('REDISTOGO_URL'), size: 10, network_timeout: 5 }
-end
+# Sidekiq.configure_server do |config|
+#   config.redis = { url: ENV.fetch('REDISTOGO_URL'), size: 10, network_timeout: 5 }
+# end
 
 Sidekiq::Extensions.enable_delay!
