b2f526fcecf06152d01e6b1fc4c6de5d587610c1
Jason Mullings <wjasonmullings@gmail.com>
2021-06-12 19:36:11 -0400

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Price.create(price: 200)
+Price.create(price: 0)
 
-User.create(email:'lemondsun@gmail.com',telegram: '', price_id: 5)
+User.create(email:'lemondsun@gmail.com',telegram: '', price_id: 1)
